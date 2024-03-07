66845328569705c465ee489cd014ebf9c86d8fe3
panivita <panivita3@gmail.com>
2024-03-07 22:26:17 +0100

    added confetti

--- src/components/GameDashboard.tsx
+++ src/components/GameDashboard.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import Data from '../data/data';
 import Card from './Card';
+import confetti from 'canvas-confetti';
 
 interface ICard {
   id: number;
@@ -17,12 +19,34 @@ const GameBoard = () => {
   const [secondCard, setSecondCard] = useState<ICard | null>(null);
   const [stopFlip, setStopFlip] = useState(false);
   const [won, setWon] = useState(0);
+  let requestId: number;
+
+  const frame = () => {
+    const end = Date.now() + 5000;
+    confetti({
+      particleCount: 7,
+      angle: 60,
+      spread: 55,
+      origin: { x: 0 },
+    });
+
+    confetti({
+      particleCount: 7,
+      angle: 120,
+      spread: 55,
+      origin: { x: 1 },
+    });
+    if (won === 0) return;
+    if (Date.now() < end) {
+      requestId = requestAnimationFrame(frame);
+    }
+  };
 
   const startNewGame = () => {
     setTimeout(() => {
       const randomOrderArray = Data.sort(() => 0.5 - Math.random()).slice(0, 6);
       const randomLetters = randomOrderArray.concat(randomOrderArray);
-      console.log(randomLetters);
+      cancelAnimationFrame(requestId)
       setCardsArray(randomLetters);
       setMoves(0);
       setFirstCard(null);
@@ -32,7 +56,7 @@ const GameBoard = () => {
   };
 
   const handleSelectedCards = (item: ICard, index: number) => {
-    const itemWithIndex = {...item, index: index}
+    const itemWithIndex = { ...item, index: index };
     if (firstCard !== null && firstCard.index !== index) {
       setSecondCard(itemWithIndex);
     } else {
@@ -74,15 +98,19 @@ const GameBoard = () => {
     startNewGame();
   }, []);
 
+  useEffect(() => {
+    if (won === 6) {
+      frame();
+    } 
+  }, [frame, won]);
+
   return (
     <div className='container'>
       <div className='header'>
         <h1>Memory Game</h1>
       </div>
       <div className='board'>
-        {cardsArray.map((item: ICard, index) => {
-          console.log(item, firstCard, secondCard, item.matched)
-          return(
+        {cardsArray.map((item: ICard, index) => (
           <Card
             item={item}
             key={index}
@@ -91,7 +119,7 @@ const GameBoard = () => {
             stopflip={stopFlip}
             index={index}
           />
-        )})}
+        ))}
       </div>
 
       {won !== 6 ? (
